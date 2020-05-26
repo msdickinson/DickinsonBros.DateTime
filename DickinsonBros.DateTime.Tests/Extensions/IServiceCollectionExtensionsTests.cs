2106a06ff0c69288045c6be6ed712eabcdb70db4
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-26 13:57:42 -0700

    Added Extension

---
+++ DickinsonBros.DateTime.Tests/Extensions/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using DickinsonBros.DateTime.Abstractions;
+using DickinsonBros.DateTime.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DickinsonBros.DateTime.Tests.Extensions
+{
+    [TestClass]
+    public class IServiceCollectionExtensionsTests
+    {
+        [TestMethod]
+        public void AddDateTimeService_Should_Succeed()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+            // Act
+            serviceCollection.AddDateTimeService();
+            // Assert
+            Assert.IsTrue(serviceCollection.Any(serviceDefinition => serviceDefinition.ServiceType == typeof(IDateTimeService) &&
+                                                       serviceDefinition.ImplementationType == typeof(DateTimeService) &&
+                                                       serviceDefinition.Lifetime == ServiceLifetime.Singleton));
+        }
+    }
+}
---
+++ DickinsonBros.DateTime/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DickinsonBros.DateTime.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DickinsonBros.DateTime.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDateTimeService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.TryAddSingleton<IDateTimeService, DateTimeService>();
+            return serviceCollection;
+        }
+    }
+}
